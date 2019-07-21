afdaf2f09b9309f4b4d2a0786d0a52b1924e8afd
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-07-21 01:34:08 -0400

    added logic to calculate match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,10 @@
-var friendsData = require("../data/friendsData");
+var friends = require("../data/friends.js");
 
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     // display a JSON of all possible friends
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
@@ -13,6 +14,21 @@ module.exports = function(app) {
 
   // determine the users most compatible friend... 
   // compatibility logic function() {
+    function findMatch() {
+      friends.forEach(function(friend) {
+        for (var i = 0; i < friend.scores.length; i++) {
+
+        }
+      })
+    }
+
+    var calcDifference = function(arr1, arr2) {
+      totalDifference = 0;
+      for (let i in arr1) {
+        totalDifference += Math.abs(arr1[i] - arr2[i]);
+      }
+      return totalDifference;
+    }
       // convert each user's results into an array of numbers
       // compare the difference between current user's scores against those from other users
       // add up the differences to calculate the totalDifference
