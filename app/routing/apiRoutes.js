cad31ea8d3e832afd701d5834f65222ee7cf7772
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-07-21 20:56:13 -0400

    fixed modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,53 @@
+// DEPENDENCIES
 var friends = require("../data/friends.js");
 
 
+// ROUTING
 module.exports = function(app) {
+
+  // GET request to display list of all possible friends on the API
   app.get("/api/friends", function(req, res) {
-    // display a JSON of all possible friends
     res.json(friends);
   });
 
+  
+  // POST request to handle incoming survey results and compatibility logic
   app.post("/api/friends", function(req, res) {
-    // handle incoming survey results
-    // handle compatibility logic
+
+    var match = {
+      name: "",
+      photo: "",
+      difference: 100
+    }
+
+
+    friends.forEach(function(friend) {
+      console.log(friends[i].name);
+
+      var scores = [];
+      var totalDifference = 100;
+
+      for (var i = 0; i < friend.scores.length; i++) {
+        scores.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
+      }
+
+      totalDifference = scores.reduce(function(total, num) {
+        return total + num;
+      }, 0)
+
+      if (totalDifference < difference) {
+        difference = totalDifference;
+        match.name = friend.name;
+        match.photo = friend.photo;
+      }
+    });
+
+    res.json(match);
+
+    friends.push(req.body);
   });
 
   // determine the users most compatible friend... 
-  // compatibility logic function() {
-    function findMatch() {
-      friends.forEach(function(friend) {
-        for (var i = 0; i < friend.scores.length; i++) {
-
-        }
-      })
-    }
-
-    var calcDifference = function(arr1, arr2) {
-      totalDifference = 0;
-      for (let i in arr1) {
-        totalDifference += Math.abs(arr1[i] - arr2[i]);
-      }
-      return totalDifference;
-    }
       // convert each user's results into an array of numbers
       // compare the difference between current user's scores against those from other users
       // add up the differences to calculate the totalDifference
