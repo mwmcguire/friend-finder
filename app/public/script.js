afac37604f7334f047a612361b82bd374aaec3d1
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-07-21 21:05:47 -0400

    script.js fix

--- app/public/script.js
+++ app/public/script.js
@@ -1,23 +1,3 @@
-// Chosen CSS
-var config = {
-  ".chosen-select": {},
-  ".chosen-select-deselect": {
-      allow_single_deselect: true
-  },
-  ".chosen-select-no-single": {
-      disable_search_threshold: 10
-  },
-  ".chosen-select-no-results": {
-      no_results_text: "Oops, nothing found!"
-  },
-  ".chosen-select-width": {
-      width: "95%"
-  }
-};
-for (var selector in config) {
-  $(selector).chosen(config[selector]);
-}
-
 
 // Capture form inputs
 $("#submit").on("click", function(event) {
